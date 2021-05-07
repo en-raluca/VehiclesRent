1f432fe64769c4f72b0ad052404a06b183963b4e
Pintea Bogdan <pinteaionutb@yahoo.com>
2021-05-07 18:23:09 +0300

    Datawriter DataReader, sa facut pachetul manger si Clasele Manager pentru Vehicle, Car si Client

---
+++ src/manager/ClientManager.java
@@ -0,0 +1,31 @@
+package manager;
+
+import model.Client;
+import model.Rent;
+
+public class Manager {
+    //UPDATE
+    public void updateNrOfkm(){
+
+    }
+    public void updatePricePerDay(){
+
+    }
+    public void updateAvailability(){
+
+    }
+    public void updateClientEmail(){
+
+    }
+    public void updateIfRentedCarIsReturned(){
+
+    }
+
+    //Find Search
+    public Rent findRent(int rereservationId){
+
+    }
+    public Client findClient(int id){
+
+    }
+}
